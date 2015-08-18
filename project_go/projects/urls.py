a8e431736d4166a9b6da970a0ddd25dd9e405692
Sharon <sharon.tang@isotoma.com>
2015-08-18 15:23:57 +0100

    FINALLY, you can now add rewards and pledge rewards to a project

--- project_go/projects/urls.py
+++ project_go/projects/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^manage/$', views.index, name='manage'),
     url(r'^create/$', views.create, name='create'),
     url(r'^edit/(?P<project_id>\d+)/$', views.edit, name='edit'),
+    url(r'^pledgerewards/(?P<project_id>\d+)/$', views.pledgerewards, name='pledgerewards'),
     url(r'^details/$', views.details, name='details'),
 ]
--- project_go/projects/views.py
+++ project_go/projects/views.py
@@ -40,7 +40,45 @@ def edit(request, project_id=None):
             if project_edit_form.is_valid():
                 project_edit_form.save()
                 
-        if 'reward' in request.POST:
+        #if 'reward' in request.POST:
+        #    reward_edit_form = RewardEditAddForm(data=request.POST, initial={}, prefix="reward")
+        #    if reward_edit_form.has_changed():
+        #        if reward_edit_form.is_valid():
+        #            r = reward_edit_form.save(commit = False)
+        #            r.project = Project.objects.get(id = project_id)
+        #            r.save()
+       # 
+       # if 'pledge' in request.POST:
+       #     pledge_edit_form = PledgeEditAddForm(data=request.POST, initial={}, prefix="pledge", current_project = project_id)
+       #     if pledge_edit_form.has_changed():
+       #         if pledge_edit_form.is_valid():
+       #             p = pledge_edit_form.save(commit = False)
+       #             p.project = Project.objects.get(id = project_id)
+       #             p.save()
+       #             pledge_edit_form.save_m2m()
+                    
+    project_edit_form = ProjectEditCreateForm(instance=p, prefix="project")
+    #pledge_edit_form = PledgeEditAddForm(initial={}, prefix="pledge", current_project = project_id)
+    #reward_edit_form = RewardEditAddForm(initial={}, prefix="reward")
+    return render_to_response('projects/editcreate.html', {
+        'form': project_edit_form,
+        #'form2': pledge_edit_form,
+        #'form3': reward_edit_form,
+        'pledges' : Pledge.objects.filter(project__id = project_id),
+        'rewards' : Reward.objects.filter(project__id = project_id),
+        'project': p}, context_instance=RequestContext(request))
+
+def pledgerewards(request, project_id=None):
+    if project_id:
+        project = get_object_or_404(Project, pk=project_id)
+        if project.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        return redirect('/projects/create')
+    
+    if request.POST:
+                
+        #if 'reward' in request.POST:
             reward_edit_form = RewardEditAddForm(data=request.POST, initial={}, prefix="reward")
             if reward_edit_form.has_changed():
                 if reward_edit_form.is_valid():
@@ -48,7 +86,7 @@ def edit(request, project_id=None):
                     r.project = Project.objects.get(id = project_id)
                     r.save()
         
-        if 'pledge' in request.POST:
+        #if 'pledge' in request.POST:
             pledge_edit_form = PledgeEditAddForm(data=request.POST, initial={}, prefix="pledge", current_project = project_id)
             if pledge_edit_form.has_changed():
                 if pledge_edit_form.is_valid():
@@ -57,16 +95,16 @@ def edit(request, project_id=None):
                     p.save()
                     pledge_edit_form.save_m2m()
                     
-    project_edit_form = ProjectEditCreateForm(instance=p, prefix="project")
     pledge_edit_form = PledgeEditAddForm(initial={}, prefix="pledge", current_project = project_id)
     reward_edit_form = RewardEditAddForm(initial={}, prefix="reward")
-    return render_to_response('projects/editcreate.html', {
-        'form': project_edit_form,
+    pledges = Pledge.objects.filter(project__id = project_id)
+    rewards = Reward.objects.filter(project__id = project_id)
+    return render_to_response('projects/pledgerewards.html', {
         'form2': pledge_edit_form,
         'form3': reward_edit_form,
-        'pledges' : Pledge.objects.filter(project__id = project_id),
-        'rewards' : Reward.objects.filter(project__id = project_id),
-        'project': p}, context_instance=RequestContext(request))
+        'pledges' : pledges,
+        'rewards' : rewards,
+        'project': project}, context_instance=RequestContext(request))
 
 def details(request):
     return render(request, 'projects/details.html')
