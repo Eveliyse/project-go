d3bc4d7f42e545a73370ac5a9d137796df98c61a
Sharon <sharon.tang@isotoma.com>
2015-09-07 15:41:10 +0100

    removed all test immages, updated user test data, updated address display in admin app

--- project_go/users/admin.py
+++ project_go/users/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Member, Address, Country, Gender
 
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'resident',
+                    'line_1',
+                    'line_2',
+                    'town',
+                    'postcode',
+                    'country',
+                    'active')
+
 admin.site.register(Gender)
 admin.site.register(Member)
-admin.site.register(Address)
-admin.site.register(Country)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country)    
